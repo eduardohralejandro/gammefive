73357f9482468de51e55f41bed2a67a7af4a903b
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-09-11 19:45:08 +0200

    new config heroku

--- routes/email.js
+++ routes/email.js
@@ -6,42 +6,31 @@ router.get('/api', async (req, res) => {
 	await res.status(200).send({ express: 'Express app' });
 });
 
-router.post(
-	'/api/contact',
-	[
-		check('name')
-			.isLength({ max: 30, min: 2 })
-			.escape()
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(422).json({ errors: errors.array() });
-		} else {
-			sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+router.post('/api/contact', async (req, res) => {
+	const errors = validationResult(req);
 
-			const email = req.body.email;
-			const name = req.body.name;
-			const message = req.body.message;
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-			const msg = {
-				to: process.env.GAMMEFIVE_EMAIL,
-				from: email,
-				subject: 'Email from gammefive',
-				text: `name: ${name};
+	const email = req.body.email;
+	const name = req.body.name;
+	const message = req.body.message;
+
+	const msg = {
+		to: process.env.GAMMEFIVE_EMAIL,
+		from: email,
+		subject: 'Email from gammefive',
+		text: `name: ${name};
 		 email:${email}; message:${message}`
-			};
+	};
 
-			await sgMail
-				.send(msg)
-				.then(result => {
-					res.status(200).send(result);
-				})
-				.catch(error => {
-					res.status(400).send(error);
-				});
-		}
-	}
-);
+	await sgMail
+		.send(msg)
+		.then(result => {
+			res.status(200).send(result);
+		})
+		.catch(error => {
+			res.status(400).send(error);
+		});
+});
 
 module.exports = router;
