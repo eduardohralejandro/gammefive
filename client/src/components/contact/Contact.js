f287bcf6e56a58f94e3723772fecff0210332148
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-09-11 20:20:42 +0200

    new config heroku

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,107 +1,84 @@
 import React, { Component, Fragment } from "react";
-import axios from "axios";
-import Helmet from "react-helmet";
-import styles from "./contact.module.scss";
+import axios from 'axios'
+import Helmet from 'react-helmet'
+import styles  from './contact.module.scss'
 class Contact extends Component {
-  state = {
-    name: "",
-    message: "",
-    email: "",
-    alertMessage: "",
-    sucessMessage: ""
-  };
-  sendData = async e => {
-    const { name, email, message } = this.state;
-
-    e.preventDefault();
-
-    const data = {
-      name,
-      email,
-      message
-    };
-
-    const testEmail = new RegExp(
-      /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g
-    ).test(email);
-
-    if (name.length < 1 || name.length === 1) {
-      this.setState({
-        alertMessage: "your name must contain at least two characters"
-      });
-    } else if (!testEmail) {
-      this.setState({ alertMessage: "you must write a valid email" });
-    } else {
-      await axios
-        .post("/api/contact", data)
-        .then(res => {
-          if (res.status === 200) {
-            this.setState({
-              sucessMessage:
-                "Thanks for your message, I will get back to you soon",
-              alertMessage: ""
-            });
-          }
-        })
-        .catch(error => {
-          console.log("important error", error);
-        });
+    state = {
+        name: "",
+        message: "",
+        email: "",
+        alertMessage: "",
+        sucessMessage:""
+        
     }
-  };
-  handleOnChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-  render() {
+    sendData = async (e) => {
+
+        const {name, email, message } = this.state
+        
+        e.preventDefault()
+
+        const data = {
+            name,
+            email,
+            message
+        }
+
+        const testEmail = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(email)
+       
+        if (name.length < 1 || name.length === 1) {
+            this.setState({alertMessage: 'your name must contain at least two characters'})
+        } else if (!testEmail) {
+            this.setState({alertMessage:'you must write a valid email'})
+        } else {
+        await axios.post('/api/contact', data).then(res => {
+            if (res.status === 200) {
+                    
+                    this.setState({sucessMessage:'Thanks for your message, I will get back to you soon', alertMessage:''})
+                }
+        }).catch(error => {
+                console.log('important error', error)
+            })
+        }
+
+ 
+       
+   
+    }
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    render() {
     return (
-      <Fragment>
-        <Helmet>
+        <Fragment>
+            
+            <Helmet>
           <style>
             {
               "body {  background: url(https://svgshare.com/i/Eox.svg) no-repeat center fixed; background-size: cover; min-height: -webkit-fill-available;"
             }
           </style>
         </Helmet>
-        <div>
-          <form className={styles.formContact} onSubmit={e => this.sendData(e)}>
-            <h1>Hey !</h1>
-            <h3>Let's talk'</h3>
-            <div className={styles.inputs}>
-              <input
-                onChange={e => this.handleOnChange(e)}
-                name="name"
-                type="text"
-                placeholder="name"
-              />
-              <input
-                onChange={e => this.handleOnChange(e)}
-                name="email"
-                type="text"
-                placeholder="email"
-              />
+            <div>
+                <form className={styles.formContact} onSubmit={(e) => this.sendData(e)}>
+                    <h1>Hey !</h1>
+                    <h3>Let's talk'</h3>
+                    <div className={styles.inputs}>
+                <input onChange={(e) => this.handleOnChange(e)} name="name" type='text' placeholder='name' />
+                <input onChange={(e) => this.handleOnChange(e)} name="email" type='text' placeholder="email" />
+                    </div>
+                    <div className={styles.textArea}>
+                <textarea onChange={(e) => this.handleOnChange(e)} name="message" type='text'  placeholder='your message...'/>
+                        <button className={this.state.alertMessage.length > 0 ?
+                        styles.btnSubmitError :  styles.btnSubmit}>Submit</button>
+                        <p>{this.state.alertMessage}</p>
+                        <p style={{color:"#680fd3"}}>{this.state.sucessMessage}</p>
+                        </div>
+                </form>
+               
             </div>
-            <div className={styles.textArea}>
-              <textarea
-                onChange={e => this.handleOnChange(e)}
-                name="message"
-                type="text"
-                placeholder="your message..."
-              />
-              <button
-                className={
-                  this.state.alertMessage.length > 0
-                    ? styles.btnSubmitError
-                    : styles.btnSubmit
-                }
-              >
-                Submit
-              </button>
-              <p>{this.state.alertMessage}</p>
-              <p style={{ color: "#680fd3" }}>{this.state.sucessMessage}</p>
-            </div>
-          </form>
-        </div>
+            
       </Fragment>
     );
   }
--- routes/email.js
+++ routes/email.js
@@ -6,31 +6,46 @@ router.get('/api', async (req, res) => {
 	await res.status(200).send({ express: 'Express app' });
 });
 
-router.post('/api/contact', async (req, res) => {
-	const errors = validationResult(req);
+router.post(
+	'/api/contact',
+	[
+		check('name')
+			.isLength({ max: 30, min: 2 })
+			.escape(),
+		check('email')
+			.isEmail()
+			.trim()
+			.normalizeEmail()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({ errors: errors.array() });
+		} else {
+			sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+			const email = req.body.email;
+			const name = req.body.name;
+			const message = req.body.message;
 
-	const email = req.body.email;
-	const name = req.body.name;
-	const message = req.body.message;
-
-	const msg = {
-		to: process.env.GAMMEFIVE_EMAIL,
-		from: email,
-		subject: 'Email from gammefive',
-		text: `name: ${name};
+			const msg = {
+				to: process.env.GAMMEFIVE_EMAIL,
+				from: email,
+				subject: 'Email from gammefive',
+				text: `name: ${name};
 		 email:${email}; message:${message}`
-	};
+			};
 
-	await sgMail
-		.send(msg)
-		.then(result => {
-			res.status(200).send(result);
-		})
-		.catch(error => {
-			res.status(400).send(error);
-		});
-});
+			await sgMail
+				.send(msg)
+				.then(result => {
+					res.status(200).send(result);
+				})
+				.catch(error => {
+					res.status(400).send(error);
+				});
+		}
+	}
+);
 
 module.exports = router;
