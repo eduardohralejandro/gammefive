b228812503b773c9976a5b25c8c8f82ef53b7b4c
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-09-11 21:17:35 +0200

    new feature/ send email/ send-grid + post route refactor / var config to heroku

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
+# NEWS
+
+- New feature: send/email. I used sendgrid for my mailing plus express-validator for sanitizers
+
+# UPDATES TO COME
+
+- Login/sign up system, my idea is to build an interactive app where people can see in the future some statistics 
+- New game/ still thinking / maze creator? puzzle with DnD? or even some games to learn Javascript, feel free to give me some ideas.
+
+#You can visit the live version of the first prototype here ----> https://gammefive.herokuapp.com
+#Want to share ideas?, talk about any technical subject, contact me ---> eduardohralejandro@gmail.com
+
 # gammefive
-game application - brain games
+ Game application - brain games/arcade
 
 # Run the client and server with concurrently
 npm run dev
@@ -7,11 +19,13 @@ npm run dev
 
 # Info
 
-This application will allow you to train your brain through games.
+This application will allow you to train your brain through games/arcade.
+
+My goal is to have fun while coding and share this with other developers.
 
 These will be a series of games in one web-app, there will be more updates to come.
 
-You can visit the live version of the first prototype here ----> https://gammefive.herokuapp.com
+
 
 # Author
 
--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,84 +1,110 @@
 import React, { Component, Fragment } from "react";
-import axios from 'axios'
-import Helmet from 'react-helmet'
-import styles  from './contact.module.scss'
+import axios from "axios";
+import Helmet from "react-helmet";
+import styles from "./contact.module.scss";
+
 class Contact extends Component {
-    state = {
-        name: "",
-        message: "",
-        email: "",
-        alertMessage: "",
-        sucessMessage:""
-        
-    }
-    sendData = async (e) => {
+  state = {
+    name: "",
+    message: "",
+    email: "",
+    alertMessage: "",
+    sucessMessage: ""
+  };
 
-        const {name, email, message } = this.state
-        
-        e.preventDefault()
+  sendData = async e => {
+    const { name, email, message } = this.state;
 
-        const data = {
-            name,
-            email,
-            message
-        }
+    e.preventDefault();
 
-        const testEmail = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(email)
-       
-        if (name.length < 1 || name.length === 1) {
-            this.setState({alertMessage: 'your name must contain at least two characters'})
-        } else if (!testEmail) {
-            this.setState({alertMessage:'you must write a valid email'})
-        } else {
-        await axios.post('/api/contact', data).then(res => {
-            if (res.status === 200) {
-                    
-                    this.setState({sucessMessage:'Thanks for your message, I will get back to you soon', alertMessage:''})
-                }
-        }).catch(error => {
-                console.log('important error', error)
-            })
-        }
+    const data = {
+      name,
+      email,
+      message
+    };
 
- 
-       
-   
-    }
-    handleOnChange = (e) => {
-        this.setState({
-            [e.target.name]:e.target.value
+    const testEmail = new RegExp(
+      /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g
+    ).test(email);
+
+    if (name.length < 1 || name.length === 1) {
+      this.setState({
+        alertMessage: "your name must contain at least two characters"
+      });
+    } else if (!testEmail) {
+      this.setState({ alertMessage: "you must write a valid email" });
+    } else {
+      await axios
+        .post("/api/contact", data)
+        .then(res => {
+          if (res.status === 200) {
+            this.setState({
+              sucessMessage:
+                "Thanks for your message, I will get back to you soon",
+              alertMessage: ""
+            });
+          }
+          this.setState({ alertMessage: "", name: "", message: "", email: "" });
         })
+        .catch(error => {
+          console.log("important error", error);
+        });
     }
-    render() {
+  };
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
     return (
-        <Fragment>
-            
-            <Helmet>
+      <Fragment>
+        <Helmet>
           <style>
             {
               "body {  background: url(https://svgshare.com/i/Eox.svg) no-repeat center fixed; background-size: cover; min-height: -webkit-fill-available;"
             }
           </style>
         </Helmet>
-            <div>
-                <form className={styles.formContact} onSubmit={(e) => this.sendData(e)}>
-                    <h1>Hey !</h1>
-                    <h3>Let's talk'</h3>
-                    <div className={styles.inputs}>
-                <input onChange={(e) => this.handleOnChange(e)} name="name" type='text' placeholder='name' />
-                <input onChange={(e) => this.handleOnChange(e)} name="email" type='text' placeholder="email" />
-                    </div>
-                    <div className={styles.textArea}>
-                <textarea onChange={(e) => this.handleOnChange(e)} name="message" type='text'  placeholder='your message...'/>
-                        <button className={this.state.alertMessage.length > 0 ?
-                        styles.btnSubmitError :  styles.btnSubmit}>Submit</button>
-                        <p>{this.state.alertMessage}</p>
-                        <p style={{color:"#680fd3"}}>{this.state.sucessMessage}</p>
-                        </div>
-                </form>
-               
+        <div>
+          <form className={styles.formContact} onSubmit={e => this.sendData(e)}>
+            <h1>Hey !</h1>
+            <h3>Let's talk'</h3>
+            <div className={styles.inputs}>
+              <input
+                onChange={e => this.handleOnChange(e)}
+                name="name"
+                type="text"
+                placeholder="name"
+              />
+              <input
+                onChange={e => this.handleOnChange(e)}
+                name="email"
+                type="text"
+                placeholder="email"
+              />
             </div>
-            
+            <div className={styles.textArea}>
+              <textarea
+                onChange={e => this.handleOnChange(e)}
+                name="message"
+                type="text"
+                placeholder="your message..."
+              />
+              <button
+                className={
+                  this.state.alertMessage.length > 0
+                    ? styles.btnSubmitError
+                    : styles.btnSubmit
+                }
+              >
+                Submit
+              </button>
+              <p>{this.state.alertMessage}</p>
+              <p style={{ color: "#680fd3" }}>{this.state.sucessMessage}</p>
+            </div>
+          </form>
+        </div>
       </Fragment>
     );
   }
